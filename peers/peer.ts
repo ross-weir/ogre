a341c372b8a53280f724f82106e274d03abe5353
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-02-19 10:31:10 +1100

    add more doc

--- peers/peer.ts
+++ peers/peer.ts
@@ -5,6 +5,7 @@ import { ScorexReader, ScorexWriter } from "../io/scorex_buffer.ts";
 import { Connection } from "../net/mod.ts";
 import { Handshake, NetworkMessageHandler, PeerSpec } from "../protocol/mod.ts";
 
+/** Events emitted by `Peer`s */
 export interface PeerEvents {
   "io:bytesOut": number;
   "io:bytesIn": number;
@@ -17,6 +18,7 @@ export interface PeerOpts {
   handler: NetworkMessageHandler;
 }
 
+/** Wraps a connection to a peer and handles message sending/receiving. */
 export class Peer extends EventEmitter<PeerEvents> implements Component {
   readonly #logger: log.Logger;
   readonly #conn: Connection;
@@ -56,18 +58,31 @@ export class Peer extends EventEmitter<PeerEvents> implements Component {
     throw new Error("Method not implemented.");
   }
 
+  /**
+   * `Handshake` received from the remote peer.
+   *
+   * Returns `undefined` if we are yet to receive a handshake
+   * from the peer.
+   */
   get handshake(): Handshake | undefined {
     return this.#remoteHandshake;
   }
 
+  /**
+   * Unix timestamp of the last message received from the remote peer.
+   *
+   * Returns` undefined` if the peer is yet to send a message.
+   */
   get lastMsgTimestamp(): number | undefined {
     return this.#lastMsgTimestamp;
   }
 
+  /** Address of the remote peer in `Multiaddr` formatting. */
   get remoteAddr(): string {
     return this.#conn.remoteAddr.toString();
   }
 
+  /** Send data to the remote peer. */
   send(data: Uint8Array): Promise<void> {
     this.dispatchEvent(
       new CustomEvent("io:bytesOut", { detail: data.byteLength }),
