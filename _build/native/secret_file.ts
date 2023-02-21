5d20981dbc97f49b010c107e7e231aa5f75e850c
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-02-21 12:29:58 +1100

    update text

--- _build/native/secret_file.ts
+++ _build/native/secret_file.ts
@@ -1,4 +1,4 @@
 export const secretQuote =
   `“I’m an ogre! You know, ‘Grab your torch and pitchforks!’ Doesn’t that bother you?"
 ---
-Did you know Ogre spelt backwards is Ergo?`;
+Did you know 'Ogre' spelt backwards is 'Ergo'?`;
