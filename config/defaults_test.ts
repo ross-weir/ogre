9abe62965e5d833b6532647b3298565f30b05ad3
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-03-01 09:27:07 +1100

    add tests for default config

---
+++ config/defaults_test.ts
@@ -0,0 +1,23 @@
+import { assertEquals, assertThrows } from "../test_deps.ts";
+import {
+  defaultsForNetwork,
+  devnetDefaultConfig,
+  mainnetDefaultConfig,
+  testnetDefaultConfig,
+} from "./defaults.ts";
+
+Deno.test("[config/defaults] defaultsForNetwork returns correct defaults for devnet", () => {
+  assertEquals(defaultsForNetwork("devnet"), devnetDefaultConfig);
+});
+
+Deno.test("[config/defaults] defaultsForNetwork returns correct defaults for testnet", () => {
+  assertEquals(defaultsForNetwork("testnet"), testnetDefaultConfig);
+});
+
+Deno.test("[config/defaults] defaultsForNetwork returns correct defaults for mainnet", () => {
+  assertEquals(defaultsForNetwork("mainnet"), mainnetDefaultConfig);
+});
+
+Deno.test("[config/defaults] defaultsForNetwork throws error for incorrect network", () => {
+  assertThrows(() => defaultsForNetwork("badnet"));
+});
