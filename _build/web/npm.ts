c28f8a8b8a7d4ffd4195d1850154cba289c7872e
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-03-03 19:51:40 +1100

    remove old comment

--- _build/web/npm.ts
+++ _build/web/npm.ts
@@ -20,12 +20,6 @@ await build({
   entryPoints: ["./_build/web/entrypoint.ts"],
   outDir: "./dist/npm",
   typeCheck: false,
-  // TODO: very much want to get this working, we need to disable deno shims though to use generated npm package in web env
-  // But by disabling deno shims we fail to generate declarations because of some Deno specific functionality
-  // used in the logger module intended for use when running natively (i.e file based logging)
-  // Could create a custom no-op shim for this or come up with some other way to get this to work.
-
-  // Could I use deno shims, then shim out the nodejs imports using `mappings`?
   declaration: true,
   test: false,
   scriptModule: false, // only support ESM
