7a94f5751351951b5c928a1b193efaed0b95d222
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-04-10 16:14:35 +1000

    add test for peer query randomize

--- peers/peers_query_test.ts
+++ peers/peers_query_test.ts
@@ -1,6 +1,7 @@
 import { GetPeersMessage } from "../protocol/messages/mod.ts";
 import { createHandshakeWithVersion } from "../protocol/messages/testing.ts";
 import { assert, assertEquals } from "../test_deps.ts";
+import { Peer } from "./peer.ts";
 import { peersQuery } from "./peers_query.ts";
 import { createRandomPeer } from "./testing.ts";
 
@@ -28,4 +29,40 @@ Deno.test("[peers/peers_query] peersQuery.canHandle returns peers that can handl
 
   assertEquals(returnedPeers.length, 1);
   assert(returnedPeers[0].isEqual(validPeer));
+});
+
+Deno.test("[peers/peers_query] peersQuery.randomize returns randomly ordered peers", () => {
+  const peers: Peer[] = [];
+  const validHs = createHandshakeWithVersion("0.0.1");
+
+  for (let i = 0; i < 10; i += 1) {
+    const peer = createRandomPeer();
+    Object.defineProperty(peer, "handshake", {
+      get() {
+        return validHs;
+      },
+    });
+
+    peers.push(peer);
+  }
+
+  const msg = new GetPeersMessage();
+  const returnedPeers = peersQuery(peers)
+    .canHandle(msg)
+    .randomize()
+    .peers();
+
+  assertEquals(returnedPeers.length, peers.length);
+
+  let isEqual = true;
+
+  for (let i = 0; i < returnedPeers.length; i += 1) {
+    if (!returnedPeers[i].isEqual(peers[i])) {
+      isEqual = false;
+
+      break;
+    }
+  }
+
+  assert(!isEqual);
 });
