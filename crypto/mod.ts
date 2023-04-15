181dbbc95a719c77f9c2e9a9895b6c4e9e41e831
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-04-15 21:02:13 +1000

    start block header encoding/decoding

--- crypto/mod.ts
+++ crypto/mod.ts
@@ -1,7 +1,28 @@
+/** Digest that is 32 bytes in size */
 export type Digest32 = Uint8Array & { readonly __brand: unique symbol };
+
+export function newDigest32(buf: Uint8Array): Digest32 {
+  if (buf.length !== 32) {
+    throw new RangeError(
+      `newDigest32: input buffer must be 32 bytes in length, got: ${buf.length}`,
+    );
+  }
+
+  return buf as Digest32;
+}
 
 /**
  * AdDigest is 33 bytes in size
  * An extra byte included for tree height
  */
 export type AdDigest = Uint8Array & { readonly __brand: unique symbol };
+
+export function newAdDigest(buf: Uint8Array): AdDigest {
+  if (buf.length !== 33) {
+    throw new RangeError(
+      `newAdDigest: input buffer must be 33 bytes in length, got: ${buf.length}`,
+    );
+  }
+
+  return buf as AdDigest;
+}
--- protocol/objects/block_header.ts
+++ protocol/objects/block_header.ts
@@ -1,5 +1,10 @@
-import { AdDigest, Digest32 } from "../../crypto/mod.ts";
-import { CursorWriter } from "../../io/cursor_buffer.ts";
+import {
+  AdDigest,
+  Digest32,
+  newAdDigest,
+  newDigest32,
+} from "../../crypto/mod.ts";
+import { CursorReader, CursorWriter } from "../../io/cursor_buffer.ts";
 import { NetworkObject, ObjectId, ObjectTypeId } from "./object.ts";
 
 export enum BlockVersion {
@@ -17,8 +22,8 @@ interface BlockHeaderOpts {
   adProofRoot: Digest32;
   stateRoot: AdDigest;
   txRoot: Digest32;
-  timestamp: number;
-  nBits: number;
+  timestamp: bigint;
+  requiredDifficulty: number;
   height: number;
   extensionRoot: Digest32;
   // powSolution make optional or separate class like ref client?
@@ -32,8 +37,9 @@ export class BlockHeader implements NetworkObject {
   readonly adProofRoot: Digest32;
   readonly stateRoot: AdDigest;
   readonly txRoot: Digest32;
-  readonly timestamp: number;
-  readonly nBits: number;
+  readonly timestamp: bigint;
+  /** `nBits` in ref client */
+  readonly requiredDifficulty: number;
   readonly height: number;
   readonly extensionRoot: Digest32;
   // powSolution make optional or separate class like ref client?
@@ -47,7 +53,7 @@ export class BlockHeader implements NetworkObject {
     this.stateRoot = opts.stateRoot;
     this.txRoot = opts.txRoot;
     this.timestamp = opts.timestamp;
-    this.nBits = opts.nBits;
+    this.requiredDifficulty = opts.requiredDifficulty;
     this.height = opts.height;
     this.extensionRoot = opts.extensionRoot;
     this.votes = opts.votes;
@@ -57,9 +63,46 @@ export class BlockHeader implements NetworkObject {
     return ObjectTypeId.BlockHeader;
   }
 
-  encode(_writer: CursorWriter): void {
-    throw new Error("Method not implemented.");
+  encode(writer: CursorWriter): void {
+    writer.putInt8(this.version);
+    // parentId
+    writer.putBytes(this.adProofRoot);
+    writer.putBytes(this.txRoot);
+    writer.putBytes(this.stateRoot);
+    writer.putUint64(this.timestamp);
+    writer.putBytes(this.extensionRoot);
+    // nbits difficulty RequiredDifficulty.serialize
+    writer.putUint32(this.height);
+    writer.putBytes(this.votes);
+
+    if (this.version > BlockVersion.Initial) {
+      writer.putUint8(0);
+    }
   }
 
-  // decode
+  static decode(reader: CursorReader): BlockHeader {
+    const version = reader.getUint8();
+    // parentId
+    const adProofRoot = newDigest32(reader.getBytes(32));
+    const txRoot = newDigest32(reader.getBytes(32));
+    const stateRoot = newAdDigest(reader.getBytes(33));
+    const timestamp = reader.getUint64();
+    const extensionRoot = newDigest32(reader.getBytes(32));
+    // nbits (difficulty)
+    const height = reader.getUint32();
+    const votes = reader.getBytes(3);
+
+    return new BlockHeader({
+      version,
+      parentId: "" as ObjectId, // TODO
+      adProofRoot,
+      txRoot,
+      stateRoot,
+      timestamp,
+      extensionRoot,
+      requiredDifficulty: 0, // TODO
+      height,
+      votes,
+    });
+  }
 }
