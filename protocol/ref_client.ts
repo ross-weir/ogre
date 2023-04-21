11c67c209a337f360417b4edb6ab4cc876c2d67b
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-04-22 07:48:34 +1000

    update sync milestone comment

--- protocol/ref_client.ts
+++ protocol/ref_client.ts
@@ -4,7 +4,7 @@ import { Version } from "./version.ts";
 export const REF_CLIENT_MILESTONE = {
   /** Initial launch version */
   initial: new Version(0, 0, 1),
-  /** Sync v2 introduced */
+  /** Sync v2 introduced, determines if the client supports SyncInfoV2 based `SyncInfo` messages */
   syncV2: new Version(4, 0, 16),
   /** Delivering broken block sections */
   brokenBlockSection4017: new Version(4, 0, 17),
